0e8a5b22ae2a0fd1170cbe3174fea17b33b25064
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-03-17 02:29:58 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -195,7 +195,7 @@ async def youtube_dl_call_back(bot, update):
         asyncio.create_task(clendir(file_location))
         asyncio.create_task(clendir(thumbnail))
         await bot.edit_message_text(
-        text="<b>ᴜᴘʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✔️\n\nᴊᴏɪɴ @VJ_BOTZ</b>",
+        text="<b>ᴜᴘʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✔️\n\nᴊᴏɪɴ @synaxnetwork 🇮🇳❤️</b>",
         chat_id=update.message.chat.id,
         message_id=update.message.id,
         disable_web_page_preview=True)
