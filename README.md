c746456ea12733349df4beee79d399c5db6f17c1
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-03-16 12:39:49 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,85 @@
+<p align="center">
+  <img src="https://graph.org/file/41c24a9826bfd8c2a1011.jpg" alt="Urluploaderxv1bot Logo">
+</p>
+<h1 align="center">
+  Synax Url Uploader Bot 🇮🇳
+</h1>
+
+
+
+![Typing SVG](https://readme-typing-svg.herokuapp.com/?lines=Welcome+To+Urluploaderxv1Bot ❤️🇮🇳;A+Highly+Advance+Url+Uploader+Bot;Made+By+Tg-@synaxBots!;With+Token+Verification+Feature;Broadcast+Force+Subscribe+Feature;A+Bot+With+Fully+Advanced+Feature!;Must+Give+Credit+To+Synax+Bots;Thank+You!)
+</p>
+
+<b>[Deploy Tutorial Video](https://t.me/synaxbots)</b>
+
+
+## Features
+
+<b><details><summary>Tap On Me For Bot Features</summary>
+ 
+- [x] Upload HTTP/HTTPS as File/Video to Telegram.
+- [x] Upload YouTube & Google Drive Link and much more..
+- [x] Upload zee5, sony.live, voot and much more..
+- [x] Permanent thumbnail Support.
+- [x] Broadcast & Force Subscribe Feature Added
+- [x] Token Verification Feature Added [ Premium Feature] 
+- [x] Custom Start Message With Picture And Buttons
+
+
+</b>
+</details>
+
+## Environment Variables
+
+<b><details><summary>Tap On Me For Environment Variable</summary>
+
+- `TECH_VJ_API_ID` : Get From [my.telegram.org](https://my.telegram.org)
+- `TECH_VJ_API_HASH` : Get From [my.telegram.org](https://my.telegram.org)
+- `TECH_VJ_BOT_TOKEN` : Get From [BotFather](https://telegram.me/BotFather)
+- `TECH_VJ_BOT_USERNAME` : Your Bot Username Without @
+- `TECH_VJ_DB_URI` : Mongodb Database Url For Main Bot [Tutorial Watch Here](https://t.me/synaxnetwork)
+- `TECH_VJ_OWNER_ID` : It mean Admin/Owner Id For Broadcasting Message.
+- `TECH_VJ_LOG_CHANNEL` : Log channel id start with -100xxxxxx
+- `TECH_VJ_UPDATES_CHANNEL` : Update channel id start with -100xxxxxx
+- `TECH_VJ` : True if you want token verification else False
+- `TECH_VJ_URL` : Your Shortner Domain Url Without https://
+- `TECH_VJ_API` : Your Shortner Api
+- `TECH_VJ_TUTORIAL` : Your How To Open Link of Shortner
+
+Heroku BuildPack :-
+- `https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest`
+
+</b>
+</details>
+
+## Credit
+
+<b><details><summary>Tap On Me For See Credit</summary>
+
+💝 Credit Goes To [Clinton Abraham](https://github.com/Clinton-Abraham) For Repository.
+
+🖍️ The Token Verification System & Force Subscribe Is Added By [Tech VJ](https://telegram.me/Kingvj01) So Don't Forgot To Give Credit
+
+💖 And Thank You So Much To All Who Help In This Journey 💕
+
+Copyright ©️ [Tech VJ](https://telegram.me/synaxnetwork)
+
+</b>
+</details>
+
+## About Owner 
+
+<b><details><summary>Tap On Me For See Details Of Owner</summary>
+
+- Telegram Channel : [VJ Botz](https://telegram.me/synaxbots)
+- Contact Link : [King VJ](https://telegram.me/sanatanisynax)
+- Instagram Id Link : [Tech VJ](https://instagram.com/sanatanisynax)
+
+</b>
+</details>
+
+
+### Copyright ©️ [Tech VJ](https://telegram.me/sanatanisynax)
+
+<b>Selling This Repo Or Code Of This Repo For Money Is Strictly Prohibited 🚫</b>
+
---
+++ config.py
@@ -0,0 +1,73 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import os
+import re
+from os import environ, getenv
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+class Config(object):
+    # Bot Information 
+    TECH_VJ_BOT_TOKEN = os.environ.get("TECH_VJ_BOT_TOKEN", "7173479389:AAFUnjPWg__-kR0hGDTVfkuckJgrQdK7qSk")
+    TECH_VJ_BOT_USERNAME = os.environ.get("TECH_VJ_BOT_USERNAME", "Urluploaderxv1Bot") # Bot username without @.
+    
+    # The Telegram API things
+    TECH_VJ_API_ID = int(os.environ.get("TECH_VJ_API_ID", "24267726"))
+    TECH_VJ_API_HASH = os.environ.get("TECH_VJ_API_HASH", "7500ba8248548cc3864bd033668f9a9a")
+    
+    # the download location, where the HTTP Server runs
+    TECH_VJ_DOWNLOAD_LOCATION = "./DOWNLOADS"
+    
+    # Telegram maximum file upload size
+    TECH_VJ_MAX_FILE_SIZE = 50000000
+    TECH_VJ_TG_MAX_FILE_SIZE = 4194304000 #2097152000
+    TECH_VJ_FREE_USER_MAX_FILE_SIZE = 50000000
+    
+    # chunk size that should be used with requests
+    TECH_VJ_CHUNK_SIZE = int(128)
+    # default thumbnail to be used in the videos
+    
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    TECH_VJ_HTTP_PROXY = ""
+    
+    # maximum message length in Telegram
+    TECH_VJ_MAX_MESSAGE_LENGTH = 4096
+    
+    # set timeout for subprocess
+    TECH_VJ_PROCESS_MAX_TIMEOUT = 3600
+    
+    # your telegram account id
+    TECH_VJ_OWNER_ID = int(os.environ.get("TECH_VJ_OWNER_ID", "6231550362")) 
+    TECH_VJ_SESSION_NAME = "VJ-URL-UPLOADER-BOT"
+    
+    # database uri (mongodb)
+    TECH_VJ_DATABASE_URL = os.environ.get("TECH_VJ_DATABASE_URL", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
+    TECH_VJ_MAX_RESULTS = "50"
+
+    # channel information
+    TECH_VJ_LOG_CHANNEL = int(os.environ.get("TECH_VJ_LOG_CHANNEL", "-1002132398644")) # your log channel id and make bot admin in log channel with full right 
+    
+    # if you want force subscribe then give your channel id below else leave blank
+    tech_vj_update_channel = environ.get('TECH_VJ_UPDATES_CHANNEL', '-1001969679920') # your update channel id and make bot admin in update channel with full right
+    TECH_VJ_UPDATES_CHANNEL = int(tech_vj_update_channel) if tech_vj_update_channel and id_pattern.search(tech_vj_update_channel) else None  
+    
+    # Url Shortner Information 
+    TECH_VJ = bool(environ.get('TECH_VJ', False)) # Set False If you want shortlink off else True
+    TECH_VJ_URL = environ.get('TECH_VJ_URL', 'link.vipurl.in') # your shortlink url domain or url without https://
+    TECH_VJ_API = environ.get('TECH_VJ_API', 'b448ce021d547cc49ea844f03feaa6c988231af6') # your url shortner api
+    TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/Toutorialbysynax/3")
+
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
